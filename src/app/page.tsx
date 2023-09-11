089d6e1004e85dad54d947eaeb3000474b0a201a
hyeonji <hyun09100407@gmail.com>
2023-09-11 15:44:53 +0900

    메인 글 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import Image from 'next/image'
 import { useEffect, useRef } from 'react'
 import { gsap } from 'gsap'
 import { ScrollTrigger } from 'gsap/ScrollTrigger' // ScrollTrigger 가져오기
@@ -9,25 +8,25 @@ import { ScrollTrigger } from 'gsap/ScrollTrigger' // ScrollTrigger 가져오기
 gsap.registerPlugin(ScrollTrigger)
 
 export default function Home() {
-  const redRef = useRef<HTMLDivElement>(null)
-  const yellowRef = useRef<HTMLDivElement>(null)
+  const firstRef = useRef<HTMLDivElement>(null)
+  const secondRef = useRef<HTMLDivElement>(null)
 
   useEffect(() => {
     // ScrollTrigger를 사용하여 스크롤 애니메이션 만들기
-    gsap.to(redRef.current, {
+    gsap.to(firstRef.current, {
       scrollTrigger: {
-        trigger: redRef.current,
-        start: 'top top', // 시작 위치 설정
+        trigger: firstRef.current,
+        start: 'top center', // 시작 위치 설정
         end: 'bottom 150px', // 끝나는 위치 설정
         pin: '#red-content',
       },
     })
 
-    gsap.to(yellowRef.current, {
+    gsap.to(secondRef.current, {
       scrollTrigger: {
-        trigger: yellowRef.current,
+        trigger: secondRef.current,
         start: 'top center', // 시작 위치 설정
-        end: '+=500', // 끝나는 위치 설정
+        end: '+=300', // 끝나는 위치 설정
         pin: '#yellow-content',
       },
     })
@@ -37,21 +36,22 @@ export default function Home() {
     <main className='mg-0 p-0 box-border'>
       <section className='w-full h-[120vh] overflow-hidden'>
         <div className=' w-[110vw] h-[120vh] box-border'>
-          <div className='relative'>
+          <div className='flex flex-col items-center justify-center w-full h-1/2'>
+            <div className='text-white text-3xl font-extrabold w-full h-full flex flex-col justify-center items-center'>
+              <span>기술과 데이터로</span>
+              <span>이동의 문제를 해결합니다.</span>
+            </div>
             <img
               src='/images/tech-bg.png'
-              className='w-full h-[120vh] absolute top-0 bottom-0 right-0 left-0'
+              className='w-[100vw] h-[120vh] absolute top-0 bottom-0 right-0 left-0 -z-10'
             />
-            <div className='text-white text-bold-extra absolute top-0 bottom-0 right-0 left-0 '>
-              ㅇㅇㅇㅇㅇ
-            </div>
           </div>
         </div>
       </section>
-      <div ref={redRef} className=' w-[110vw] h-[120vh] bg-red-500'>
+      <div ref={firstRef} className=' w-[110vw] h-[120vh] bg-red-500'>
         <div id='red-content'>두번째 컨텐트</div>
       </div>
-      <div ref={yellowRef} className=' w-[110vw] h-[120vh] bg-yellow-500'>
+      <div ref={secondRef} className=' w-[110vw] h-[120vh] bg-yellow-500'>
         <div id='yellow-content'>세번째 컨텐트</div>
       </div>
     </main>
